3793cedbc5b86674a9463a3eb12bd9fb9a6229f5
spoorthyElla <spoorthy18vaignana@gmail.com>
2015-06-21 17:10:38 -0500

    Edited signature of getReverse()

--- src/main/java/org/home/interviewprep/linkedlist/AdvancedLinkedListOperations.java
+++ src/main/java/org/home/interviewprep/linkedlist/AdvancedLinkedListOperations.java
@@ -3,8 +3,6 @@ package org.home.interviewprep.linkedlist;
 public abstract class AdvancedLinkedListOperations<T> extends LinkedList_1<T> {
 	abstract Node<T> getMiddleElement();
 	
-	void getReverse(LinkedList<T> temp) {
-		// TODO Auto-generated method stub
+	abstract void getReverse(LinkedList<T> temp) ;		
 		
-	}
 }
