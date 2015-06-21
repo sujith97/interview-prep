369a04c0199b1f6527a1ece3c5b2ea5d4a05fef3
spoorthyElla <spoorthy18vaignana@gmail.com>
2015-06-21 16:57:35 -0500

    Added getReverse() method to Abstract Class

--- src/main/java/org/home/interviewprep/linkedlist/AdvancedLinkedListOperations.java
+++ src/main/java/org/home/interviewprep/linkedlist/AdvancedLinkedListOperations.java
@@ -2,4 +2,5 @@ package org.home.interviewprep.linkedlist;
 
 public abstract class AdvancedLinkedListOperations<T> extends LinkedList_1<T> {
 	abstract Node<T> getMiddleElement();
+	abstract LinkedList<T> getReverse();
 }
